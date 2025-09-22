c39a9f9e280b25103302ae457fa6894afd3f3e8f
henriettearch <mbabazih251@gmail.com>
2025-09-22 18:46:43 +0200

    Add files via upload

---
+++ Assignment_22_09_2025/TestLoanCalculator.java
@@ -0,0 +1,9 @@
+package com.form2;
+
+public class TestLoanCalculator {
+	public static void main(String[] args) {
+			LoanCalculator lc=new LoanCalculator();
+
+	}
+
+}
---
+++ Assignment_22_09_2025/TestRegistrationForm.java
@@ -0,0 +1,9 @@
+package com.form1;
+
+public class TestRegistrationForm {
+	public static void main(String[] args) {
+		RegistrationForm re=new RegistrationForm();
+
+	}
+
+}
